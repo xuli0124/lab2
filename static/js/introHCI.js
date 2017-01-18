ebf7f4aa0222fc3374440eeb9d76475a6a5b3552
Xu Li <xuli0124@gmail.com>
2017-01-18 15:24:51 -0800

    submission for lab2

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,14 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("a.thumbnail").click(projectClick);
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e){
+	e.preventDefault();
+	$(this).css("background-color","#7fff00")
 }
